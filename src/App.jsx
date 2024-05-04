f7a142a1eca290c7fc5aee45fd579e9f4b3ea213
Fahim <anik.fahim07@gmail.com>
2024-05-04 12:53:34 +0600

    connect to github

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Component/Home/Header/Header"
+
+
+function App() {
+ 
+  return (
+    <>
+     <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/Home/Header/Header.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import logo from '../../../assets/Image/Logo/logo.svg'
+import { HiBars3BottomRight } from "react-icons/hi2";
+import { IoMdCloseCircle } from "react-icons/io";
+
+const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen); // Toggle menu visibility on button click
+      };
+    const navList=<>
+    <li className='hover:bg-[#333333] text-white p-2 rounded-lg'>Home</li>
+    <li className='hover:bg-[#333333] text-white p-2 rounded-lg'>About Us</li>
+    <li className='hover:bg-[#333333] text-white p-2 rounded-lg'>Properties</li>
+    <li className='hover:bg-[#333333] text-white p-2 rounded-lg'>Service</li>
+    <button className='bg-[#000000] text-white p-3 rounded-lg  flex md:hidden mb-2'>
+                Contact Us
+               </button>
+    </>
+    return (
+        <nav className='flex justify-between  px-11 py-6 items-center bg-[#262626]  text-white'>
+            {/* logo */}
+            <div className='flex items-center justify-center gap-2 text-2xl font-medium '>
+              <img src={logo} alt="" />
+              <h3>Skyline</h3>
+            </div>
+            {/* nav list */}
+            <div className={`absolute md:hidden md:min-h-fit bg-[#262626] min-h-[40vh] left-0 top-[11%]  w-full md:w-auto flex items-center px-5 justify-center ${isOpen ? 'block' : 'hidden'}`}>
+             <ul className='flex flex-col md:flex-row items-center justify-center md:gap-[5vw] gap-8 '>
+                {navList}
+             </ul>
+            </div>
+            <div className='hidden md:flex'>
+             <ul className='flex flex-col md:flex-row items-center justify-center md:gap-[5vw] gap-8 '>
+                {navList}
+             </ul>
+            </div>
+            {/* button */}
+            <div>
+               <button  className='bg-[#000000] text-white p-3 rounded-lg  hidden md:flex'>
+                Contact Us
+               </button>
+            </div>
+            <div className='flex items-center md:hidden '>
+                <button className='text-3xl cursor-pointer ' onClick={toggleMenu}>
+               
+                {isOpen ? <IoMdCloseCircle /> : <HiBars3BottomRight />}
+                </button>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
